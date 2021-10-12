1d392e9faa66b5b6d8b15bf4f6365af72b26b034
Justin Walsh <contact.me@thejustinwalsh.com>
2021-10-12 16:37:14 -0700

    feat: pwa support for offline use

---
+++ serviceWorker.js
@@ -0,0 +1,24 @@
+const cache = "zzfx-1.1.8";
+const assets = [
+  "/",
+  "/index.html",
+  "/facivon.png",
+  "/ZzFXMicro.min.js",
+  "/wav.js",
+];
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(cache).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+});
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+});
